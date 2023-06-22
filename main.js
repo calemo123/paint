5f5779748d3e15681b095eceefdca91a9c18aac4
calemo123 <111438923+calemo123@users.noreply.github.com>
2023-06-22 10:31:37 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,38 +1,26 @@
 canvas = document.getElementById("myCanvas");
 ctx = canvas.getContext("2d");
 
-ctx.beginPath();
-ctx.strokeStyle = "red";
-ctx.lineWidth = 1;
-ctx.rect(150, 143, 430, 200);
-ctx.stroke();
+function myMouseDown(e)
+{
+color = document.getElementById("color").value;
+widthLine = document.getElementById("widthLine").value;
+radius = document.getElementById("radius").value;
+MouseEvent = "mouseDown";
+}
+function mouseMove(e)
+{
+ positionMouseX = e.clientX - canvas.offsetLeft;
+ positionMouseY = e.clientY - canvas.offsetTop;
 
-ctx.beginPath();
-ctx.strokeStyle = "blue";
-ctx.lineWidth = 5
-ctx.arc(250, 210, 40, 0, 2 *Math.PI);
-ctx.stroke();
-
-ctx.beginPath();
-ctx.strokeStyle = "yellow";
-ctx.lineWidth = 5
-ctx.arc(300, 250, 40, 0, 2 *Math.PI);
-ctx.stroke();
-
-ctx.beginPath();
-ctx.strokeStyle = "black";
-ctx.lineWidth = 5
-ctx.arc(350, 210, 40, 0, 2 *Math.PI);
-ctx.stroke();
-
-ctx.beginPath();
-ctx.strokeStyle = "green";
-ctx.lineWidth = 5
-ctx.arc(400, 250, 40, 0, 2 *Math.PI);
-ctx.stroke();
-
-ctx.beginPath();
-ctx.strokeStyle = "red";
-ctx.lineWidth = 5
-ctx.arc(450, 210, 40, 0, 2 *Math.PI);
-ctx.stroke();
+ if(MouseEvent == "mouseDown") 
+ {
+ console.log("Current position of x and y coordinates = ");
+ console.log("x = " + PositionMouseX + "y = " + PositionMouseY );
+ ctx.beginPath();
+ ctx.strokeStyle = color;
+ ctx.lineWidth = widthLine;
+ ctx.arc(positionMouseX, PositionMouseY, radius, 0, 2 *Math.PI);
+ ctx.strokeStyle();
+ }
+}
